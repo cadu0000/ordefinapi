becde9a59b634268b0d7905a28251260aeb9ad78
cadu0000 <edukadu1000@gmail.com>
2025-08-18 15:34:53 -0300

    small changes

--- src/main/java/com/infra/postgre/init.sql
+++ src/main/java/com/infra/postgre/init.sql
@@ -6,11 +6,11 @@ CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 CREATE TYPE transaction_type AS ENUM ('INCOME', 'EXPENSE');
 
 CREATE TABLE IF NOT EXISTS transaction_category (
-    category_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    category_id INT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS "user" (
+CREATE TABLE IF NOT EXISTS "users" (
     user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(256) NOT NULL,
     email VARCHAR(256) UNIQUE NOT NULL,
@@ -22,7 +22,8 @@ CREATE TABLE IF NOT EXISTS transaction (
     amount DOUBLE PRECISION NOT NULL,
     date DATE NOT NULL,
     type transaction_type NOT NULL,
-    category_id UUID NOT NULL REFERENCES transaction_category(category_id) ON DELETE CASCADE,
+    category_id INT NOT NULL REFERENCES transaction_category(category_id) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES users(user_id),
     description VARCHAR(255)
 );
 
