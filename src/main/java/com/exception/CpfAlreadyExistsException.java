16ffb095c1c250acff72dc805fa0e995d15bc2c7
cadu0000 <edukadu1000@gmail.com>
2025-08-22 17:02:08 -0300

    add exception

---
+++ src/main/java/com/exception/CpfAlreadyExistsException.java
@@ -0,0 +1,11 @@
+package com.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT) // 409
+public class CpfAlreadyExistsException extends RuntimeException {
+    public CpfAlreadyExistsException(String message) {
+        super(message);
+    }
+}
